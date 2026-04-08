c7e9ed2359cdcab331052caddedb3508cfdaddf1
gigi686 <giovannagabriely1603@gmail.com>
2026-04-08 12:30:43 +0000

    alteraçao

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
-let likeCount = 0;
-let curtido = false; // flag booleana
-
-function curtir() {
-
- if(curtido == false){
-    likeCount++;
-    curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
- }else{
-    likeCount--;
-    curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
- }
-
+const likeBtn = document.querySelector(".likeBtn"); 
+const dislikeBtn = document.querySelector(".dislikeBtn");
+ const countSpan = document.querySelector(".likeCount"); 
+ 
+ let count = 0; 
+ let liked = false; 
+ let disliked = false;
   
-}
-
-document.getElementById("likeBtn").addEventListener("click", curtir);
+ likeBtn.addEventListener("click", function() 
+ { if (!liked) { 
+   liked = true; 
+ likeBtn.classList.add("liked"); 
+ count++; if (disliked) { disliked = false;
+ dislikeBtn.classList.remove("disliked"); } } else { liked = false; likeBtn.classList.remove("liked"); count--; } countSpan.textContent = count; }); dislikeBtn.addEventListener("click", function() { if (!disliked) { disliked = true; dislikeBtn.classList.add("disliked"); if (liked) { liked = false; likeBtn.classList.remove("liked"); count--; } } else { disliked = false; dislikeBtn.classList.remove("disliked"); } countSpan.textContent = count; });|
